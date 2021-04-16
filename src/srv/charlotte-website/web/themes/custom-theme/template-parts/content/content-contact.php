b6d60f3c099b2acc1618dd420155d60324f1b5b4
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2021-04-16 09:02:30 +0100

    Add contact information

--- src/srv/charlotte-website/web/themes/custom-theme/template-parts/content/content-contact.php
+++ src/srv/charlotte-website/web/themes/custom-theme/template-parts/content/content-contact.php
@@ -2,6 +2,14 @@
 
 <?php get_header();?>
 
-  contact2
+  <div class="contact-page">
+
+    <i class="far fa-envelope"></i>
+
+    <p>I am always looking for new projects and creative opportunities, so feel free to contact me at</p>
+
+    <h1>charlotte.bunyan3@gmail.com</h1>
+
+  </div>
 
 <?php get_footer();?>

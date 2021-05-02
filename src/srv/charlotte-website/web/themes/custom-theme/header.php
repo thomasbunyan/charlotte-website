0910b006dc89ac4035599ee714347a346a65fbb3
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2021-05-02 21:59:35 +0100

    Update meta tag

--- src/srv/charlotte-website/web/themes/custom-theme/header.php
+++ src/srv/charlotte-website/web/themes/custom-theme/header.php
@@ -3,8 +3,8 @@
   
   <head>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
   </head>
 

0333a114e254cc8d1f2f79921ecd5c3f5bc24bbc
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2021-05-03 18:57:37 +0100

    Fix footer title and subtitle

--- src/srv/charlotte-website/web/themes/custom-theme/footer.php
+++ src/srv/charlotte-website/web/themes/custom-theme/footer.php
@@ -2,9 +2,9 @@
 
   <footer class="noselect">
 
-    <h1><?php echo get_field("footer_title", 10); ?></h1>
+    <h1><?php echo get_field("footer_title", get_page_by_path('about')->ID); ?></h1>
 
-    <p class="subtitle"><?php echo get_field("footer_subtitle", 10); ?></p>
+    <p class="subtitle"><?php echo get_field("footer_subtitle", get_page_by_path('about')->ID); ?></p>
 
     <div class="contact-button">
       <a href=<?php echo get_permalink(get_page_by_path('contact')->ID) ?>>Contact</a>
